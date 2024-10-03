003150fb44aef3f8e9c06426a0f50eb95e807d47
Jesus Pinar <not@email.com>
2024-10-03 15:26:46 +0200

    Add center text

--- src/scenes/startScene.ts
+++ src/scenes/startScene.ts
@@ -1,18 +1,23 @@
 import { GameContext } from "../types/gameContext";
 
 export function startScene(k: GameContext) {
-	// Add bg
+	// Añadir fondo
 	k.add([k.sprite("background"), k.scale(3)]);
 
-	k.add([k.text("Planet Shooter", { size: 48 })]);
+	const titleSize = 48;
+	const startButtonSize = 32;
 
-	const startButton = k.add([
-		k.text("Start", { size: 32 }),
-		k.pos(0, 50),
-		k.area({ cursor: "pointer" }),
-		{ isStartButton: true },
-	]);
+	const title = k.add([k.text("PLANET SHOOTER", { size: titleSize }), k.pos(0, 0)]);
 
+	const titleWidth = title.text.length * (titleSize * 0.6);
+	title.pos.x = (k.width() - titleWidth) / 2;
+	title.pos.y = k.height() / 2 - 60;
+
+	const startButton = k.add([k.text("Start", { size: startButtonSize }), k.area({ cursor: "pointer" }), k.pos(0, 0)]);
+
+	const buttonWidth = startButton.text.length * (startButtonSize * 0.6);
+	startButton.pos.x = (k.width() - buttonWidth) / 2;
+	startButton.pos.y = k.height() / 2 + 60;
 	startButton.onClick(() => {
 		k.go("game");
 	});
