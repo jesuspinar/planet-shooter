3b26bbc58ec7ff3706a3b81176d15245ef7ce325
Jesus Pinar <not@email.com>
2024-10-03 14:03:02 +0200

    Add button to restart

--- src/scenes/gameOverScene.ts
+++ src/scenes/gameOverScene.ts
@@ -2,10 +2,19 @@ import { GameContext } from "../types/gameContext";
 
 export function gameOverScene(k: GameContext) {
 	return (score: number) => {
-		k.add([k.text(`Game Over!\nYour score: ${score}`), k.pos(k.width() / 2, k.height() / 2)]);
+		// Add background
+		k.add([k.sprite("background"), k.scale(3)]);
+		k.add([k.text(`Game Over!\nYour score: ${score}`)]);
 
-		k.onKeyPress("space", () => {
-			k.go("game");
-		});
+		const startButton = k.add([
+      k.text("Restart", { size: 32 }),
+      k.pos(0, 150),
+      k.area({ cursor: "pointer" }),
+      { isStartButton: true },
+    ]);
+
+    startButton.onClick(() => {
+      k.go("game");
+    });
 	};
 }
