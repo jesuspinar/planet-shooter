375088b7a361fa6ba27d41ba037c19b3e7017531
Jesus Pinar <not@email.com>
2024-10-03 12:18:36 +0200

    Create planet

---
+++ src/objects/planet.ts
@@ -0,0 +1,18 @@
+import { GameObj, Vec2 } from "kaboom";
+
+import { PLANET_SPEED } from "../config";
+import { GameContext } from "../types/gameContext";
+import { PlanetType } from "../types/planetType";
+
+export function createPlanet(k: GameContext, pos: Vec2, type: PlanetType): GameObj {
+	return k.add([
+		k.sprite(type.sprite),
+		k.pos(pos),
+		k.area(),
+		k.scale(0.1),
+		k.color(k.rgb(type.color[0], type.color[1], type.color[2])),
+		k.move(k.DOWN, PLANET_SPEED),
+		"planet",
+		{ value: type.value },
+	]);
+}
