279c477df7fc3075e160913381d0e136984e0c0e
Jesus Pinar <not@email.com>
2024-10-11 17:22:19 +0200

    Update pos

--- src/scenes/startScene.ts
+++ src/scenes/startScene.ts
@@ -1,31 +1,25 @@
 import { GameContext } from "../types/gameContext"
 
 export function startScene(k: GameContext) {
-  const titleSize = 48
+  const titleSize = 38
   const startButtonSize = 32
 
-  const title = k.add(
+  k.add(
     [
       k.text("PLANET SHOOTER", { size: titleSize }),
-      k.pos(0, 0)
+      k.pos(k.width() / 2, k.height() / 2),
+      k.anchor("center"),
     ]
   )
-
-  const titleWidth = title.text.length * (titleSize * 0.6)
-  title.pos.x = (k.width() - titleWidth) / 2
-  title.pos.y = k.height() / 2 - 60
 
   const startButton = k.add(
     [
       k.text("Start", { size: startButtonSize }),
       k.area({ cursor: "pointer" }),
-      k.pos(0, 0)
+      k.pos(k.width() / 2, k.height() / 2 + 60),
+      k.anchor("center"),
     ]
   )
-
-  const buttonWidth = startButton.text.length * (startButtonSize * 0.6)
-  startButton.pos.x = (k.width() - buttonWidth) / 2
-  startButton.pos.y = k.height() / 2 + 60
 
   startButton.onClick(() => k.go("game"))
 }
