6679dff92bdc5f1dd709f00cee736c7e78ff2685
Jesus Pinar <not@email.com>
2024-10-11 17:18:36 +0200

    Update pos

--- src/scenes/gameOverScene.ts
+++ src/scenes/gameOverScene.ts
@@ -2,17 +2,31 @@ import { GameContext } from "../types/gameContext"
 
 export function gameOverScene(k: GameContext) {
   return (score: number) => {
-    k.add([k.text(`Game Over!\nYour score: ${score}`)])
-
-    const startButton = k.add([
-      k.text("Restart", { size: 32 }),
-      k.pos(0, 150),
+    // Score text
+    k.add(
+      [
+        k.text(`${score}`),
+        k.pos(k.width() / 2, k.height() / 2 - 75),
+        k.anchor("center"),
+      ]
+    )
+    // Game over text
+    k.add(
+      [
+        k.text("Game Over!"),
+        k.pos(k.width() / 2, k.height() / 2 - 35),
+        k.anchor("center"),
+      ]
+    )
+    // Restart button
+    const restartButton = k.add([
+      k.text("Restart?", { size: 32 }),
       k.area({ cursor: "pointer" }),
+      k.pos(k.width() / 2, k.height() / 2 + 30),
+      k.anchor("center"),
       { isStartButton: true },
     ])
 
-    startButton.onClick(() => {
-      k.go("game")
-    })
+    restartButton.onClick(() => k.go("game"))
   }
 }
