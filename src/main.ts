eadb7f4f78ae53c5df1c18ce9ea0bf1daeeaaac2
Jesus Pinar <not@email.com>
2024-10-11 15:13:09 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import kaplay from "kaplay";
+import "kaplay/global";
+
+const k = kaplay()
+
+k.loadSprite("bean", "sprites/bean.png")
+
+k.add([
+	k.pos(120, 80),
+	k.sprite("bean"),
+])
+
+k.onClick(() => k.addKaboom(k.mousePos()))
